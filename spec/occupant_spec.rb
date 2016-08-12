f055a187aadec5b3c65ad9e6bf0cbdb7a88af2ca
Aniela M Wolkonowski <anielamw@gmail.com>
2016-08-12 15:14:09 -0400

    created the Occupant Class

--- spec/occupant_spec.rb
+++ spec/occupant_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
+
 describe Occupant do
-  it "is a pending example" do
+  let!(:occupant1)  {Occupant.new("Matt", "Dunning")}
+  it "initializes an occupant with the first name 'Matt' and a last name 'Dunning'" do
+    expect(occupant1.first_name).to eq("Matt")
+    expect(occupant1.last_name).to eq("Dunning")
   end
 end
